5bab5a397c20953a6d024c96d922133c0c89157a
kolitha <harith.13@itfac.mrt.ac.lk>
2022-10-08 07:37:43 -0500

    ini project

---
+++ core/train.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from config import MODEL_PATH
+
+# if pkl file cannot file create the model
+print('Training Started...')
+data = load_iris()
+X = data.data
+Y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X,Y, random_state = 50, test_size = 0.25)
+clf = DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+with open(MODEL_PATH, 'wb') as f:
+    pickle.dump(clf, f)
+print('Training Completed !')
---
+++ run.py
@@ -0,0 +1,20 @@
+from http import client
+from flask import Flask, render_template
+import os
+import pymongo
+from routes import commonRoutes, predictRoute
+from utils import modelExists
+
+app = Flask(__name__, instance_relative_config=True)
+
+# Register Routes
+app.register_blueprint(commonRoutes.commonRoutes)
+app.register_blueprint(predictRoute.predictRoutes,url_prefix='/predict')
+
+# Train
+if(modelExists.checkModelExists() == False):
+    from core import train
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 3000))
+    app.run(debug=True, host='0.0.0.0', port=port)
