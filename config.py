14aa1dda7321c10d1e29e71f7e23ee57d70fd4e6
kolitha <harith.13@itfac.mrt.ac.lk>
2022-10-08 13:42:09 -0500

    update

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 MODEL_PATH = 'models/model.pkl'
 OUTPUT_PNG_PATH = 'app/static/tree.png'
+S3_BUCKET_NAME = 'tree-image-data'
+AWS_ACCESS_KEY=os.getenv('AWS_ACCESS_KEY')
+AWS_SECRET_KEY=os.getenv('AWS_SECRET_KEY')
 
--- routes/predictRoute.py
+++ routes/predictRoute.py
@@ -1,7 +1,8 @@
 from flask import Flask, Blueprint, render_template, url_for, json, request, redirect
 from config import MODEL_PATH
 import pickle
-from utils import saveToImage
+from sklearn import datasets
+from utils import saveToImage, datasets
 
 predictRoutes = Blueprint('predict', __name__)
 
@@ -10,6 +11,11 @@ def predict():
     features = [list(request.args.to_dict().values())]
     feature_cols = list(request.args.to_dict().keys())
     clf = pickle.load(open(MODEL_PATH, "rb"))
-    result = str(clf.predict(features)[0])
-    saveToImage.save(clf, feature_cols)
-    return result 
+    predictResult = clf.predict(features);
+    s3Url = saveToImage.save(clf, feature_cols)
+    return {
+        'Features': request.args.to_dict(),
+        'ClassIndex': str(predictResult),
+        'Classes': str(datasets.convertToClasses(predictResult)),
+        'ImageUrl': s3Url
+    } 
